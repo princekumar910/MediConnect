d5229394f744adaa1e5db6dd714b1d1cfe69399c
Prince Kumar <princekumar007p@gmail.com>
2024-12-15 22:37:56 +0530

    Appointement Solve

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -173,4 +173,24 @@ const bookAppointment = async (req , res)=>{
 }
 
 
-export {registerUser , loginUser ,getProfile , updateProfile , bookAppointment}
+//  api to get user appointment for frontend 
+
+const  listAppointment = async (req , res)=>{
+
+    try {
+
+        const {userId} =req.body ;
+        const appointements = await appointmentModel.find({userId})
+        
+        res.json({success : true , appointements})
+
+        
+    } catch (error) {
+        console.log(error)
+        res.json({success : false , message : error.message})
+    }
+
+}
+
+
+export {registerUser , loginUser ,getProfile , updateProfile , bookAppointment , listAppointment}
